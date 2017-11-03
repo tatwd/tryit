0678dca04235532232b0f1c3b967e35d266db8cc
tatwd <leoking9641@hotmail.com>
2017-11-03 19:18:36 +0800

    Update

--- Java/Test.java
+++ Java/Test.java
@@ -2,9 +2,9 @@ package com.tatwd.test;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Test class!");
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello Java!");
+    }
 
 }
