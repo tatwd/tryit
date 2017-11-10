326d6033b3288ae41c6f40f4247264e79632e6fb
tatwd <leoking9641@hotmail.com>
2017-11-10 12:47:51 +0800

    Update backtop.js

--- Kit/backtop/backtop.js
+++ Kit/backtop/backtop.js
@@ -6,29 +6,26 @@
     
     // 监听滑块事件 
     $.addEventListener('scroll', function() {
-        if($.scrollY > 0) {
-            backtop.classList.add('active');    // 添加 active 类添加到对象的类列表中
-        } else {
-            backtop.classList.remove('active'); // 删除
-        }
+
+        ($.scrollY > 0) ? 
+            backtop.classList.add('active') :   // 添加 active 类
+            backtop.classList.remove('active'); // 删除 active 类
+        
     }, false);
 
-    // 为 backtop下的 icon 对象添加点击事件
-    backtop.children[0].addEventListener('click', function() {
+    // 为 backtop 对象添加点击事件
+    backtop.addEventListener('click', function() {
         var x = $.scrollX;
         var y = $.scrollY;
         
         var interval = $.setInterval(function() {
 
-            if(y > 0) {
-                y = y - 150; // 每次减150
+            (y > 0) ? y = y - 150: $.clearInterval(interval); // 清除计时器
 
-                $.scrollTo(x, y);
-
-            } else {
-                $.clearInterval(interval); // 清除计时器
-            }
+            $.scrollTo(x, y);
+            
         }, 3); // 0.003s
-    }, false);
+
+    }, true);  // true 捕获阶段执行 
 
 }(window);
