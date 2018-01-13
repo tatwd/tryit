0c7764d231a38f654274e3c276915701441c8168
tatwd <leoking9641@hotmail.com>
2018-01-13 18:54:15 +0800

    Add some test files.

---
+++ Asp.Net/TestSolu2/Ui/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ui.Global" Language="C#" %>
---
+++ Asp.Net/TestSolu2/Ui/Home.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Model;
+using Bll;
+using MySql.Data.MySqlClient;
+
+namespace Ui
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GetBtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(StuIdTb.Text.Trim());
+
+            try
+            {
+                List<Student> list = StudentService.GetStudent(id);
+
+                ViewData.DataSource = list;
+                ViewData.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
