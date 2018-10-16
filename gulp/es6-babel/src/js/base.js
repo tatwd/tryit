f1bfd752c328b97397a2e70b10150f97a6ce8833
tatwd <leoking9641@hotmail.com>
2018-10-16 09:30:03 +0800

    chore: lowercase and update pkg

---
+++ gulp/es6-babel/src/js/base.js
@@ -0,0 +1,13 @@
+class Pen {
+    constructor(str) {
+        this.str = str;
+    }
+
+    logger() {
+        if(console) {
+            console.log(`${this.str} -- _king`);
+        }
+    }
+}
+
+export default Pen;  // this is a class
