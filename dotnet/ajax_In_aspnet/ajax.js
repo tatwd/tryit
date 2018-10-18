54d123c3b4b61c63edd6e7ba9179d96cf0627835
tatwd <leoking9641@hotmail.com>
2018-10-18 22:18:25 +0800

    format js demo

--- dotnet/ajax_In_aspnet/ajax.js
+++ dotnet/ajax_In_aspnet/ajax.js
@@ -1,70 +1,80 @@
-﻿const ajax = function (url, settings) {
+﻿const ajax = function(url, settings) {
+  // set default values
+  ({
+    method = 'GET',
+    responseType = '',
+    header = {},
+    timeout = 0,
+    data = null,
+    async = true
+  } = settings || {});
 
-    // set default values
-    ({ method = 'GET', responseType = '', header = {}, timeout = 0, data = null, async = true } = settings || {});
+  // get XMLHttpRequest object
+  let getXhr = () =>
+    new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
 
-    // get XMLHttpRequest object
-    let getXhr = () => new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
-
-    class DataFormater {
-        constructor(data) {
-            this._data = data;
-        }
-        
-        getJson() {
-            let __data = typeof this._data === 'object'
-            ? this._data
-            : JSON.parse(this._data);
-            
-            
-            return __data === null
-                ? __data
-                : (__data.d && __data.d !== '' ? JSON.parse(__data.d) : __data); 
-        }
-
-        getText() {
-            return this._data;
-        }
+  class DataFormater {
+    constructor(data) {
+      this._data = data;
     }
 
-    return new Promise((resolve, reject) => {
-        const xhr = getXhr();
-        
-        xhr.open(method, url, async);
+    getJson() {
+      let __data =
+        typeof this._data === 'object' ? this._data : JSON.parse(this._data);
 
-        xhr.onreadystatechange = function () {
-            if (this.readyState !== 4) return;
+      return __data === null
+        ? __data
+        : __data.d && __data.d !== ''
+          ? JSON.parse(__data.d)
+          : __data;
+    }
 
-            this.status === 200
-                // ? resolve({
-                //     _data: this.response,
-                //     getJson: function () {
-                //         let __data = typeof this._data === 'object'
-                //             ? this._data
-                //             : JSON.parse(this._data);
-                        
-                //         return __data === null
-                //             ? __data
-                //             : (__data.d && __data.d !== '' ? JSON.parse(__data.d) : __data);
-                //     },
-                //     getText: function () {
-                //         return this._data;
-                //     }
-                // })
-                ? resolve(new DataFormater(this.response))
-                : reject(new Error(this.statusText));
-        }
+    getText() {
+      return this._data;
+    }
+  }
 
-        xhr.timeout = timeout;
-        xhr.responseType = responseType;
+  return new Promise((resolve, reject) => {
+    const xhr = getXhr();
 
-        // set request header
-        for (let item in header) {
-            xhr.setRequestHeader(item, `application/${header[item]}`);
-        }
+    xhr.open(method, url, async);
 
-        xhr.send(data);
-    });
+    xhr.onreadystatechange = function() {
+      if (this.readyState !== 4) return;
+
+      this.status === 200
+        ? // ? resolve({
+          //     _data: this.response,
+          //     getJson: function() {
+          //       let __data =
+          //         typeof this._data === 'object'
+          //           ? this._data
+          //           : JSON.parse(this._data);
+
+          //       return __data === null
+          //         ? __data
+          //         : __data.d && __data.d !== ''
+          //           ? JSON.parse(__data.d)
+          //           : __data;
+          //     },
+          //     getText: function() {
+          //       return this._data;
+          //     }
+          //   })
+          resolve(new DataFormater(this.response))
+        : reject(new Error(this.statusText));
+    };
+
+    xhr.timeout = timeout;
+    xhr.responseType = responseType;
+
+    // set request header
+    for (let item in header) {
+      xhr.setRequestHeader(item, `application/${header[item]}`);
+    }
+
+    xhr.send(data);
+  });
 };
 
 window.ajax = window.ajax || ajax;
@@ -75,36 +85,36 @@ window.ajax = window.ajax || ajax;
 // const dataContainer = document.querySelector('.data-container');
 
 // let url = {
-//     test: 'test.json',
-//     aspx: '../Demo.aspx/ServerMethod'
+//   test: 'test.json',
+//   aspx: '../Demo.aspx/ServerMethod'
 // };
 
 // let settings = {
-//     get: {
-//         method: 'GET',
-//         // responseType: 'json',
-//         header: {
-//             'content-type': 'json'
-//         }
-//     },
-//     post: {
-//         method: 'POST',
-//         responseType: 'json',
-//         header: {
-//             'content-type': 'json'
-//         },
-//         data: JSON.stringify({
-//             id: 1
-//         }),
+//   get: {
+//     method: 'GET',
+//     // responseType: 'json',
+//     header: {
+//       'content-type': 'json'
 //     }
-// }
+//   },
+//   post: {
+//     method: 'POST',
+//     responseType: 'json',
+//     header: {
+//       'content-type': 'json'
+//     },
+//     data: JSON.stringify({
+//       id: 1
+//     })
+//   }
+// };
 
 // ajax(url.test, settings.get)
-// // ajax(url.aspx, settings.post)
-//     .then(response => response.getJson())
-//     .then(data => {
-//         dataContainer.innerHTML = JSON.stringify(data);
-//     })
-//     .catch(error => {
-//         dataContainer.innerHTML = error;
-//     });
+//   // ajax(url.aspx, settings.post)
+//   .then(response => response.getJson())
+//   .then(data => {
+//     dataContainer.innerHTML = JSON.stringify(data);
+//   })
+//   .catch(error => {
+//     dataContainer.innerHTML = error;
+//   });
--- dotnet/ajax_In_aspnet/main.js
+++ dotnet/ajax_In_aspnet/main.js
@@ -1,214 +1,212 @@
 ﻿var input = document.querySelector('.input-text');
 var subBtn = document.querySelector('.sub-btn');
 
+var dogo = (function() {
+  // Dogo class
+  var Dogo = function(selectorOrdom) {
+    this.element = selectorOrdom || {};
+    this.data = [];
+  };
 
-var dogo = (function () {
+  // get dom element(s) form html
+  Dogo.prototype.dom = function(selector) {
+    var that = this;
+    var dom =
+      typeof selector === 'string'
+        ? selector.startsWith('#')
+          ? document.querySelector(selector)
+          : document.querySelectorAll(selector)
+        : null;
 
-
-    // Dogo class
-    var Dogo = function (selectorOrdom) {
-        this.element = selectorOrdom || {};
-        this.data = [];
+    var Getter = function(_dom) {
+      this.dom = _dom;
     };
 
-    // get dom element(s) form html
-    Dogo.prototype.dom = function (selector) {
-        var that = this;
-        var dom = typeof selector === 'string'
-            ? (
-                selector.startsWith("#")
-                    ? document.querySelector(selector)
-                    : document.querySelectorAll(selector)
-            )
-            : null;
+    Getter.prototype.at = function(number) {
+      var ele =
+        typeof number === 'undefined' ||
+        (number < 0 || number >= this.dom.length)
+          ? null
+          : this.dom[number];
 
-        var Getter = function (_dom) {
-            this.dom = _dom;
-        };
+      // that.element = ele;
 
-        Getter.prototype.at = function (number) {
-            var ele
-                = typeof number === 'undefined' || (number < 0 || number >= this.dom.length)
-                ? null
-                : this.dom[number];
+      // ele.prototype.render = that.render;
 
-            // that.element = ele;
-
-            // ele.prototype.render = that.render;
-
-            return ele; // dogo object
-        };
-
-        Getter.prototype.all = function () {
-            return this.dom;
-        };
-
-        return new Getter(dom);
+      return ele; // dogo object
     };
 
-    // judge dom element method
-    Dogo.prototype.isDom = function (obj) {
-        return typeof HTMLElement === 'object' // the type of `HTMLElement` in Chrome is a function
-            ? (obj instanceof HTMLElement)
-            : (typeof obj === 'object' && obj.nodeType === 1 && typeof obj.nodeName === 'string');
+    Getter.prototype.all = function() {
+      return this.dom;
     };
 
-    // ajax method
-    Dogo.prototype.ajax = function (setting) {
-        var that = this;
+    return new Getter(dom);
+  };
 
-        var Ajax = function (setting) {
-            this.xhr = new XMLHttpRequest();
-            this.setting = setting || {};
-        };
+  // judge dom element method
+  Dogo.prototype.isDom = function(obj) {
+    return typeof HTMLElement === 'object' // the type of `HTMLElement` in Chrome is a function
+      ? obj instanceof HTMLElement
+      : typeof obj === 'object' &&
+          obj.nodeType === 1 &&
+          typeof obj.nodeName === 'string';
+  };
 
-        Ajax.prototype.get = function () {
+  // ajax method
+  Dogo.prototype.ajax = function(setting) {
+    var that = this;
 
-            this.xhr.open("GET", this.setting.url);
-            return that;
-        };
-
-        Ajax.prototype.post = function () {
-            this.xhr.open("POST", this.setting.url);
-            this.xhr.setRequestHeader("Content-Type", `application/${this.setting.contentType}`); // json type
-            this.xhr.send(JSON.stringify(this.setting.data));
-
-            return this;
-        };
-
-        Ajax.prototype.render = function (setting) {
-
-            this.xhr.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-
-                    var jsonStr = this.responseText;
-                    var jsonObj = {};
-
-                    if (jsonStr) {
-                        var _data = JSON.parse(jsonStr).d;
-                        jsonObj = _data ? JSON.parse(_data) : null;
-                    }
-
-                    jsonObj && that.data.push(jsonObj);
-
-                    // var index = setting.tmp.indexOf('{');
-
-                    var element = that.dom(setting.ele).all();
-                    var template = document.createElement('template');
-
-                    if (that.data.length == 0) {
-                        element.innerHTML = "NULL";
-                        return;
-                    }
-
-                    setting.tmp = setting.tmp.replace(/{{.*}}/, that.data[0].Name);
-                    template.innerHTML = setting.tmp;
-                    
-                    var clone = document.importNode(template.content, true);
-                    // element.appendChild(clone);
-                    element.innerHTML = clone.textContent || "NULL";
-
-                    that.data = [];
-                }
-            };
-        };
-
-        return new Ajax(setting);
+    var Ajax = function(setting) {
+      this.xhr = new XMLHttpRequest();
+      this.setting = setting || {};
     };
 
-    return new Dogo();
+    Ajax.prototype.get = function() {
+      this.xhr.open('GET', this.setting.url);
+      return that;
+    };
+
+    Ajax.prototype.post = function() {
+      this.xhr.open('POST', this.setting.url);
+      this.xhr.setRequestHeader(
+        'Content-Type',
+        `application/${this.setting.contentType}`
+      ); // json type
+      this.xhr.send(JSON.stringify(this.setting.data));
+
+      return this;
+    };
+
+    Ajax.prototype.render = function(setting) {
+      this.xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var jsonStr = this.responseText;
+          var jsonObj = {};
+
+          if (jsonStr) {
+            var _data = JSON.parse(jsonStr).d;
+            jsonObj = _data ? JSON.parse(_data) : null;
+          }
+
+          jsonObj && that.data.push(jsonObj);
+
+          // var index = setting.tmp.indexOf('{');
+
+          var element = that.dom(setting.ele).all();
+          var template = document.createElement('template');
+
+          if (that.data.length == 0) {
+            element.innerHTML = 'NULL';
+            return;
+          }
+
+          setting.tmp = setting.tmp.replace(/{{.*}}/, that.data[0].Name);
+          template.innerHTML = setting.tmp;
+
+          var clone = document.importNode(template.content, true);
+          // element.appendChild(clone);
+          element.innerHTML = clone.textContent || 'NULL';
+
+          that.data = [];
+        }
+      };
+    };
+
+    return new Ajax(setting);
+  };
+
+  return new Dogo();
 })();
 
 var $ = dogo;
 
-subBtn.onclick = function () {
-    var text = input.value;
+subBtn.onclick = function() {
+  var text = input.value;
 
-    $.ajax({
-        url: "./DemoA.aspx/ServerMethod",
-        contentType: 'json;charset=utf-8',
-        data: {
-            id: Number(input.value)
-        }
-    }).post().render({
-        ele: '#app',
-        tmp: `<p>The message from server is, "Hello, I am {{ Name }}."</p>`
+  $.ajax({
+    url: './DemoA.aspx/ServerMethod',
+    contentType: 'json;charset=utf-8',
+    data: {
+      id: Number(input.value)
+    }
+  })
+    .post()
+    .render({
+      ele: '#app',
+      tmp: `<p>The message from server is, "Hello, I am {{ Name }}."</p>`
     });
 };
 
 // ------------
 
-//!(function AppStart() {
+// !(function AppStart() {
+//   var template = {};
+//   var clone = {};
+//   var render = {};
+//   var app = document.querySelector('#app');
 
-//    var template = {};
-//    var clone = {};
-//    var render = {};
-//    var app = document.querySelector('#app');
+//   if (!dogo.isDomObj(app)) {
+//     console.log(`${app} is not a html element object!`);
+//     return;
+//   }
 
-//    if (!dogo.isDomObj(app)) {
-//        console.log(`${app} is not a html element object!`);
-//        return;
-//    }
+//   // add `tmp` content to `ele`
+//   render = {
+//     ele: app,
+//     tmp: `<p>this is the content of template.</p>`
+//   };
 
-//    // add `tmp` content to `ele`
-//    render = {
-//        ele: app,
-//        tmp: `<p>this is the content of template.</p>`
-//    };
+//   temp = document.createElement('template');
+//   temp.innerHTML = render.tmp;
+//   clone = document.importNode(temp.content, true);
 
-//    temp = document.createElement('template');
-//    temp.innerHTML = render.tmp;
-//    clone = document.importNode(temp.content, true);
+//   render.ele.appendChild(clone);
+// })();
 
-//    render.ele.appendChild(clone);
+// function sendXhr(data) {
+//   var xhr = new XMLHttpRequest();
 
-//})();
+//   //xhr.open("POST", "DemoA.aspx", true);
+//   //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-//function sendXhr(data) {
-//    var xhr = new XMLHttpRequest();
+//   xhr.open('POST', './DemoA.aspx/ServerMethod', true);
+//   xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8'); // json type
+//   //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-//    //xhr.open("POST", "DemoA.aspx", true);
-//    //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//   var dataJsonObj = {
+//     id: data
+//   };
 
-//    xhr.open("POST", "./DemoA.aspx/ServerMethod", true);
-//    xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8"); // json type
-//    //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//   // xhr.send("{'id': 12}");
+//   xhr.send(JSON.stringify(dataJsonObj)); // 给 WebMethod 方法传递参数
+//   // xhr.send("id=" + text);
 
-//    var dataJsonObj = {
-//        "id": data
-//    };
+//   xhr.onreadystatechange = function() {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       var jsonStr = xhr.responseText;
+//       var jsonObj = [];
+//       var responseTextDom = document.querySelector('#response-text');
 
-//    // xhr.send("{'id': 12}");
-//    xhr.send(JSON.stringify(dataJsonObj)); // 给 WebMethod 方法传递参数
-//    // xhr.send("id=" + text);
+//       if (jsonStr) {
+//         jsonObj = JSON.parse(jsonStr);
 
-//    xhr.onreadystatechange = function () {
-//        if (xhr.readyState == 4 && xhr.status == 200) {
+//         // console.log("jsonObj:", jsonObj);
 
-//            var jsonStr = xhr.responseText;
-//            var jsonObj = [];
-//            var responseTextDom = document.querySelector("#response-text");
+//         responseTextDom.innerHTML = jsonObj.d; // jsonStr;
+//       } else {
+//         //console.log("There is no the student!");
 
-//            if (jsonStr) {
-//                jsonObj = JSON.parse(jsonStr);
+//         responseTextDom.innerHTML = 'There is no the student!';
+//       }
 
-//                // console.log("jsonObj:", jsonObj);
+//       // responseTextDom.innerHTML = jsonObj.Name;
+//       // console.log(xhr.responseText);
+//     }
+//   };
+// }
 
-//                responseTextDom.innerHTML = jsonObj.d; // jsonStr;
-//            } else {
-//                //console.log("There is no the student!");
+// subBtn.onclick = function() {
+//   var text = input.value;
 
-//                responseTextDom.innerHTML = "There is no the student!";
-//            }
-
-//            // responseTextDom.innerHTML = jsonObj.Name;
-//            // console.log(xhr.responseText);
-//        }
-//    };
-//}
-
-//subBtn.onclick = function () {
-//    var text = input.value;
-
-//    sendXhr(text);
-//};
+//   sendXhr(text);
+// };
