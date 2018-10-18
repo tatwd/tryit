1adc9ce7d6acee3865d12092afa53aeb7ce3e00b
tatwd <leoking9641@hotmail.com>
2018-10-18 23:04:10 +0800

    test and build

--- gulp/browser-sync/gulpfile.js
+++ gulp/browser-sync/gulpfile.js
@@ -1,36 +1,40 @@
 // load plugins
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create(); 
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
 // static server to listen css + html files
 gulp.task('serve', function() {
-    //place code for your default tast here
-    browserSync.init({
-        server: {
-            baseDir:"./src",   // base directory
-            index: "demo.html" // default open the file
-        }    
-    });
+  //place code for your default tast here
+  browserSync.init({
+    server: {
+      baseDir: './src', // base directory
+      index: 'demo.html' // default open the file
+    }
+  });
 
-    gulp.watch('src/css/*.css', ['css']);
-    gulp.watch('src/js/*.js', ['js']);
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+  gulp.watch('src/css/*.css', ['css']);
+  gulp.watch('src/js/*.js', ['js']);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
 // listen css files
 gulp.task('css', function() {
-    return gulp.src('src/css/*.css')
-        .pipe(browserSync.reload({  // loads the changed css
-            stream: true
-        }));
+  return gulp.src('src/css/*.css').pipe(
+    browserSync.reload({
+      // loads the changed css
+      stream: true
+    })
+  );
 });
 
 // listen js files
-gulp.task('js', function(){
-    return gulp.src('src/js/*.js')
-        .pipe(browserSync.reload({ // loads the changed js
-            stream: true
-        }));
+gulp.task('js', function() {
+  return gulp.src('src/js/*.js').pipe(
+    browserSync.reload({
+      // loads the changed js
+      stream: true
+    })
+  );
 });
 
 gulp.task('default', ['serve']); // serve then defalut
--- gulp/browser-sync/src/js/test.js
+++ gulp/browser-sync/src/js/test.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    var p = document.getElementsByTagName('p')[0];
-    p.innerHTML = 'Welcome to use Browser-sync with Gulp!';
-    console.log('load me!');
+  var p = document.getElementsByTagName('p')[0];
+  p.innerHTML = 'Welcome to use Browser-sync with Gulp!';
+  console.log('load me!');
 };
