8b2490dff1150a28535d825b95adc9daa486dab0
tatwd <leoking9641@hotmail.com>
2018-10-18 22:34:51 +0800

    styles: format code style

--- gulp/es6-babel/gulpfile.js
+++ gulp/es6-babel/gulpfile.js
@@ -1,26 +1,31 @@
-const
-    gulp = require('gulp'),
-    babel = require('gulp-babel'),
-    browserify = require('gulp-browserify');
+const gulp = require('gulp'),
+  babel = require('gulp-babel'),
+  browserify = require('gulp-browserify');
 
 // es6 task
 gulp.task('es6', () => {
-    return gulp.src('src/js/*.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('dist/js/'));
+  return gulp
+    .src('src/js/*.js')
+    .pipe(
+      babel({
+        presets: ['es2015']
+      })
+    )
+    .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('browserify', ['es6'], () => {
-    gulp.src('dist/js/test.js')
-        .pipe(browserify({
-            //
-        }))
-        .pipe(gulp.dest('dist/'))
-})
+  gulp
+    .src('dist/js/test.js')
+    .pipe(
+      browserify({
+        //
+      })
+    )
+    .pipe(gulp.dest('dist/'));
+});
 
 // default task
 gulp.task('default', ['browserify'], () => {
-    gulp.watch('src/js/*.js', ['browserify']); // watch js
+  gulp.watch('src/js/*.js', ['browserify']); // watch js
 });
--- gulp/es6-babel/src/js/base.js
+++ gulp/es6-babel/src/js/base.js
@@ -1,13 +1,13 @@
 class Pen {
-    constructor(str) {
-        this.str = str;
-    }
+  constructor(str) {
+    this.str = str;
+  }
 
-    logger() {
-        if(console) {
-            console.log(`${this.str} -- _king`);
-        }
+  logger() {
+    if (console) {
+      console.log(`${this.str} -- _king`);
     }
+  }
 }
 
-export default Pen;  // this is a class
+export default Pen; // this is a class
