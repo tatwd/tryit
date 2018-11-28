02a089155c9ee1d1115ee39a05d374dca757f09e
tatwd <leoking9641@hotmail.com>
2018-11-28 23:16:51 +0800

    clang: use tab instead of space

--- clang/pointer2.c
+++ clang/pointer2.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-    int arr[5];
-
-    arr[0] = 1;
-    arr[4] = 5;
-    printf("%d ... %d\n", arr[1], arr[4]);
-
-    arr[-1] = 0;
-    arr[5] = 6;
-    printf("arr[-1]: %d, arr[5]: %d\n", arr[-1], arr[5]);
-
-    arr[3] = 128;
+	int arr[5];
+	
+	arr[0] = 1;
+	arr[4] = 5;
+	printf("%d ... %d\n", arr[1], arr[4]);
+	
+	arr[-1] = 0;
+	arr[5] = 6;
+	printf("arr[-1]: %d, arr[5]: %d\n", arr[-1], arr[5]);
+	
+	arr[3] = 128;
 /*
 Now:
          高位 <------------------------- 低位
@@ -24,7 +24,7 @@ arr[4]:  00000000 00000000 00000000 00000111  <- 0X60FF   高位
 
 */
 
-    ((short *)arr)[6] = 2;
+	((short *)arr)[6] = 2;
 /*
 Now:
                     高位 <------- 低位
@@ -41,7 +41,7 @@ Now:
 
 So, the `arr[3]` is `2`.
 */
-    printf("arr[3]: %d\n", arr[3]);
-
-    return 0;
+	printf("arr[3]: %d\n", arr[3]);
+	
+	return 0;
 }
