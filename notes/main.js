27ecd668e6193b5f0662e03735506737c1121efa
tatwd <leoking9641@hotmail.com>
2019-01-08 20:30:46 +0800

    notes: update html of item

--- notes/main.js
+++ notes/main.js
@@ -7,30 +7,30 @@ $(function() {
 
   function succeeded(data) {
     var badges = {
-      'TODO': 'badge-secondary',
-      'READING': 'badge-info',
-      'FINISHED': 'badge-success'
+      TODO: 'badge-secondary',
+      READING: 'badge-info',
+      FINISHED: 'badge-success'
     };
     var authorTypes = {
-      'WRITER': '作者',
-      'TRANSLATOR': '译者'
+      WRITER: '作者',
+      TRANSLATOR: '译者'
     };
     var h = function(i) {
       var authors = i.authors.reduce((r, i) => {
         var t = authorTypes[i.type];
         r += `${t}: ${i.name} `;
         return r;
-      }, '')
+      }, '');
 
       return `
 <li class="list-group-item">
-  <h5 class="lead">${i.title}</h5>
+  <p class="lead">${i.title}</p>
   <p class="text-muted">
     <span>${authors}</span>
     <span>${i.press}</span>
     <span>${i.publish_at ? i.publish_at + ' 版' : ''}</span>
   </p>
-  <p class="mb-1">
+  <p class="mb-1 text-muted">
     <span class="badge badge-pill ${badges[i.status]}">${i.status}</span>
     <span>${i.begin_at || '-'} to ${i.end_at || '-'}</span>
   </p>
