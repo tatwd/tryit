715ff1f760b160dbd109da565c60667d6104f8d8
tatwd <leoking9641@hotmail.com>
2019-01-08 17:55:08 +0800

    notes: add reading books data and site

---
+++ notes/main.js
@@ -0,0 +1,48 @@
+$(function() {
+  var books = $('#books');
+
+  function failed(err) {
+    console.log(err.status, err.statusText);
+  }
+
+  function succeeded(data) {
+    var badges = {
+      'TODO': 'badge-secondary',
+      'READING': 'badge-info',
+      'FINISHED': 'badge-success'
+    };
+    var authorTypes = {
+      'WRITER': '作者',
+      'TRANSLATOR': '译者'
+    };
+    var h = function(i) {
+      var authors = i.authors.reduce((r, i) => {
+        var t = authorTypes[i.type];
+        r += `${t}: ${i.name} `;
+        return r;
+      }, '')
+
+      return `
+<li class="list-group-item">
+  <h5 class="lead">${i.title}</h5>
+  <p class="text-muted">
+    <span>${authors}</span>
+    <span>${i.press}</span>
+    <span>${i.publish_at ? i.publish_at + ' 版' : ''}</span>
+  </p>
+  <p class="mb-1">
+    <span class="badge badge-pill ${badges[i.status]}">${i.status}</span>
+    <span>${i.begin_at || '-'} to ${i.end_at || '-'}</span>
+  </p>
+</li>`;
+    };
+
+    data.forEach(i => {
+      var html = $(books).append(h(i));
+    });
+  }
+
+  $.get('books.json')
+    .done(succeeded)
+    .fail(failed);
+});
