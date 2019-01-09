60905c51c43e5d0b65ce52457b1d47747e333143
tatwd <leoking9641@hotmail.com>
2019-01-09 18:16:33 +0800

    fe: update backtop js code

--- frontend/backtop/backtop.js
+++ frontend/backtop/backtop.js
@@ -1,33 +1,33 @@
 !(function($) {
-  // 此处 $ 是 window 对象！
+  var options = {
+    el: '.backtop',
+    duration: 3, // 3ms
+    step: 150
+  };
 
-  // 获取 backtop 对象
-  var backtop = $.document.getElementsByClassName('backtop')[0];
+  var backtop = document.querySelector(options.el);
+
+  // 显示切换
+  function toggle() {
+    window.scrollY > 0
+      ? backtop.classList.add('active')
+      : backtop.classList.remove('active');
+  }
+
+  // 返回顶部
+  function back() {
+    console.log(options);
+    var y = window.scrollY;
+    var interval = window.setInterval(function() {
+      y > 0 ? (y = y - options.step) : window.clearInterval(interval); // 清除计时器
+      window.scrollTo(0, y);
+    }, options.duration);
+  }
 
   // 监听滑块事件
-  $.addEventListener(
-    'scroll',
-    function() {
-      $.scrollY > 0
-        ? backtop.classList.add('active') // 添加 active 类
-        : backtop.classList.remove('active'); // 删除 active 类
-    },
-    false
-  );
+  toggle();
+  window.addEventListener('scroll', toggle, false);
 
   // 为 backtop 对象添加点击事件
-  backtop.addEventListener(
-    'click',
-    function() {
-      var x = $.scrollX;
-      var y = $.scrollY;
-
-      var interval = $.setInterval(function() {
-        y > 0 ? (y = y - 150) : $.clearInterval(interval); // 清除计时器
-
-        $.scrollTo(x, y);
-      }, 3); // 0.003s
-    },
-    true
-  ); // true 捕获阶段执行
-})(window);
+  backtop.addEventListener('click', back, true); // true 捕获阶段执行
+})();
