114902da4df0b2ad2e8fcf779b1e23a9cd1ac838
tatwd <leoking9641@hotmail.com>
2019-02-21 18:29:41 +0800

    golang: add gorm example

---
+++ golang/gorm/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+)
+
+// PmMaterial is a model of `pm_material` table
+type PmMaterial struct {
+	gorm.Model
+	ID        string     `json:"id"`
+	Title     string     `json:"title"`
+	CreatedAt *time.Time `json:"created_at"`
+}
+
+func main() {
+	// 建立连接
+	str := "server=localhost;user id=sa;password=root123;database=test_erp_sys;"
+	// str := "sqlserver://sa:root123@localhost:1433?database=test_erp_sys"
+	db, err := gorm.Open("mssql", str)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	// 生成数据库表
+	db.AutoMigrate(&PmMaterial{})
+	if !db.HasTable("pm_material") {
+		fmt.Println("no pm_material")
+		return
+	}
+
+	// 新增一条数据
+	now := time.Now().UTC()
+	db.Create(&PmMaterial{ID: "1235", Title: "hello world", CreatedAt: &now})
+
+	// 查询
+	// var item PmMaterial
+	// db.First(&item, "1235")
+	// fmt.Println(item)
+
+	// 更新
+	// db.Model(&item).Update("Title", "Hi")
+
+	// 删除
+	// db.Delete(&item)
+}
