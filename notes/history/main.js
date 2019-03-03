32803db2c6873833a37d33f2c9ffd67d33c99405
tatwd <leoking9641@hotmail.com>
2019-03-03 14:21:12 +0800

    notes(history): add dynasties info

---
+++ notes/history/main.js
@@ -0,0 +1,47 @@
+$(function() {
+  var BASE_API_URL = 'https://api.ctext.org/';
+  var dynastiesDom = $('#dynasties');
+  var renderDynasties = function(data) {
+    var s = '';
+    $.each(data, function(idx, item) {
+      // console.log(item);
+      var from = Number(item.yearfrom);
+      var to = Number(item.yearto);
+      var span = Math.abs(to - from) + 1;
+      if (from < 0) from = '前 ' + -from;
+      if (to < 0) to = '前 ' + -to;
+      s += `<div class="col-sm-4 pb-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="lead">${item.title}</h5>
+                <p class="card-text">
+                  ${from} ~ ${to} 年 <br>
+                  <small>历时：${span} 年</small>
+                </p>
+              </div>
+            </div>
+          </div>`;
+    });
+    dynastiesDom.html(s);
+  };
+  var getDynasties = function() {
+    var old_data = localStorage.getItem('dynasties');
+    old_data && renderDynasties(JSON.parse(old_data));
+    $.get(BASE_API_URL + 'getdynasties?if=zh&remap=gb')
+      .done(function(data, type, xhr) {
+        var lastmodified = localStorage.getItem('getdynasties_saved_at');
+        var now = Date.now();
+        if (!lastmodified || now - lastmodified > 1 * 3600 * 1000) {
+          localStorage.setItem('getdynasties_saved_at', now);
+          localStorage.setItem('dynasties', JSON.stringify(data.dynasties));
+        }
+        !old_data && renderDynasties(data.dynasties);
+      })
+      .fail(function(err) {
+        console.log(err);
+      });
+  };
+  setTimeout(() => {
+    getDynasties();
+  }, 500);
+});
