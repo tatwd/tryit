4d75777ec91ce9b70d7b9e7d513b3cb69eedfead
tatwd <leoking9641@hotmail.com>
2019-06-01 23:59:49 +0800

    db(mysql): add manage_user.sql

---
+++ database/mysql/02_manage_user.sql
@@ -0,0 +1,23 @@
+-- 1. 使用 GRANT
+-- GRANT <privileges> 
+-- ON <database_name>.<table_name> 
+-- TO 'user_name'@'host_name'
+-- IDENTIFIED BY 'user_password';
+-- 
+-- 2. 在 mysql.user 表中添加一条记录
+-- 不同的版的 user 表字段可能不同
+
+
+-- 赋予名为 jaron 的用户在 localhost 主机上访问 test_db 数据库
+-- 并具有 SELECT,INSERT,UPDATE,DELETE,CREATE,DROP 权限
+
+-- 方式一:
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP -- privileges
+ON test_db.* -- all tables of `database_name`
+TO 'jaron'@'localhost'
+IDENTIFIED BY 'test123';
+
+-- 方式二:
+INSERT INTO mysql.user(Host,User,authentication_string,Select_priv,Insert_priv,Update_priv,Delete_priv,Create_priv,Drop_priv)
+VALUES('localhost', 'jaron', PASSWORD('test123'), 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
+FLUSH PRIVILEGES; -- 重读授权表
