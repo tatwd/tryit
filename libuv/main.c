559e7b6ac3957edb7f6648cb26280e34ade9e2c1
tatwd <leoking9641@hotmail.com>
2020-04-15 22:27:19 +0800

    libuv: add uvcat, onchange, tcp-echo-server

--- libuv/main.c
+++ libuv/main.c
@@ -1,64 +1,88 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
 #include <uv.h>
 
-#define BUF_MAX 1024
+#define DEFAULT_PORT 8080
+#define DEFAULT_BACKLOG 128
 
-void on_open(uv_fs_t *req);
-void on_read(uv_fs_t *req);
-void on_write(uv_fs_t *req);
+uv_loop_t *loop;
+struct sockaddr_in addr;
 
-char buffer[BUF_MAX];
-uv_buf_t iov;
-uv_fs_t open_req;
-uv_fs_t read_req;
-uv_fs_t write_req;
+typedef struct {
+	uv_write_t req;
+	uv_buf_t buf;
+} write_req_t;
 
-void on_open(uv_fs_t *req)
+void free_write_req(uv_write_t *req)
 {
-	assert(req == &open_req);
-	if (req->result >= 0) {
-		// 成功打开文件则开始读取数据
-		iov = uv_buf_init(buffer, sizeof(buffer));
-		uv_fs_read(uv_default_loop(), &read_req, req->result,
-		           &iov, 1, -1, on_read);
-	} else {
-		fprintf(stderr, "error opening file: %s\n", uv_strerror((int)req->result));
-	}
+	write_req_t *wr = (write_req_t *)req;
+	free(wr->buf.base);
+	free(wr);
 }
 
-void on_read(uv_fs_t *req)
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
 {
-	if (req->result < 0) {
-		fprintf(stderr, "read error: %s\n", uv_strerror(req->result));
-	} else if (req->result == 0) {
-		uv_fs_t close_req;
-		/* synchronous */
-		uv_fs_close(uv_default_loop(), &close_req, open_req.result, NULL);
-	} else if (req->result > 0) {
-		iov.len = req->result;
-		uv_fs_write(uv_default_loop(), &write_req, 1, &iov, 1, -1, on_write);
-	}
+	buf->base = (char *)malloc(suggested_size);
+	buf->len = suggested_size;
 }
 
-void on_write(uv_fs_t *req)
+void echo_write(uv_write_t *req, int status)
 {
-	if (req->result < 0) {
-		fprintf(stderr, "write error: %s\n", uv_strerror((int)req->result));
+	if (status) {
+		fprintf(stderr, "Write error %s\n", uv_strerror(status));
+	}
+	free_write_req(req);
+}
+
+void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
+{
+	if (nread > 0) {
+		write_req_t *req = (write_req_t *)malloc(sizeof(write_req_t));
+		req->buf = uv_buf_init(buf->base, nread);
+		uv_write((uv_write_t *)req, client, &req->buf, 1, echo_write);
+		return;
+	}
+	if (nread < 0) {
+		if (nread != UV_EOF)
+			fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+		uv_close((uv_handle_t *)client, NULL);
+	}
+
+	free(buf->base);
+}
+
+void on_connection(uv_stream_t *server, int status)
+{
+	if (status < 0) {
+		fprintf(stderr, "New connection error %s\n", uv_strerror(status));
+		return;
+	}
+
+	printf("A new connection!\n");
+
+	uv_tcp_t *client = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
+	uv_tcp_init(loop, client);
+	if (uv_accept(server, (uv_stream_t *)client) == 0) {
+		uv_read_start((uv_stream_t *)client, alloc_buffer, echo_read);
 	} else {
-		uv_fs_read(uv_default_loop(), &read_req, open_req.result,
-		           &iov, 1, -1, on_read);
+		uv_close((uv_handle_t *)client, NULL);
 	}
 }
 
 int main(int argc, char **argv)
 {
-	uv_fs_open(uv_default_loop(), &open_req, argv[1], O_RDONLY, 0, on_open);
-	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+	loop = uv_default_loop();
 
-	/* must call uv_fs_re_cleanup in fs req */
-	uv_fs_req_cleanup(&open_req);
-	uv_fs_req_cleanup(&read_req);
-	uv_fs_req_cleanup(&write_req);
-	return 0;
+	uv_tcp_t server;
+	uv_tcp_init(loop, &server);
+
+	uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
+
+	uv_tcp_bind(&server, (const struct sockaddr *)&addr, 0);
+	int r = uv_listen((uv_stream_t *)&server, DEFAULT_BACKLOG, on_connection);
+	if (r) {
+		fprintf(stderr, "Listen error %s\n", uv_strerror(r));
+		return 1;
+	}
+	return uv_run(loop, UV_RUN_DEFAULT);
 }
