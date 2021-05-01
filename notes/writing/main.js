6e1dcd297a862c5e3fee184fe2ac12ea0feebf6e
tatwd <leoking9641@hotmail.com>
2021-05-01 16:56:25 +0800

    update

--- notes/writing/main.js
+++ notes/writing/main.js
@@ -1,19 +1,19 @@
-$(function() {
+$(function () {
   var $poetries = $("#poetries");
 
   function fetchPoetries(cb) {
     $.get("./poetries.json")
-      .done(function(data, type, xhr) {
+      .done(function (data, type, xhr) {
         cb && cb(data, xhr);
       })
-      .fail(function(res) {
+      .fail(function (res) {
         console.log(res);
         $poetries.html("获取数据失败，可以打开控制台查询错误信息！");
       });
   }
 
   function reducePoetries(poetries) {
-    return poetries.reduce(function(acc, cur, idx) {
+    return poetries.reduce(function (acc, cur, idx) {
       var key = new Date(cur.created_at).getFullYear();
       if (!acc[key]) acc[key] = [];
       acc[key].push({ data: cur, index: idx });
@@ -23,7 +23,7 @@ $(function() {
 
   function renderHTML(year, poetries) {
     var s = "";
-    $.each(poetries, function(index, item) {
+    $.each(poetries, function (index, item) {
       index = item.index;
       item = item.data;
       s += `
@@ -33,7 +33,7 @@ $(function() {
     title="写于${item.created_at}"
     href="./poetry.html?key=${encodeURI(index)}"
   >${item.title}</a>
-  <small class='badge--light'>${item.type === 1 ? "[旧体]" : ""}</small>
+  <small class='badge--light'>[${item.type || "N/A"}]</small>
 </p>`;
     });
     return `
@@ -53,10 +53,10 @@ $(function() {
     $el.html(s || "无");
   }
 
-  window.setTimeout(function() {
+  window.setTimeout(function () {
     var data = JSON.parse(localStorage.getItem("poetries")) || [];
     render($poetries, data);
-    fetchPoetries(function(data, xhr) {
+    fetchPoetries(function (data, xhr) {
       var lastModified = localStorage.getItem("last_modified");
       var nowLastModified = new Date(xhr.getResponseHeader("last-modified"))
         .getTime()
